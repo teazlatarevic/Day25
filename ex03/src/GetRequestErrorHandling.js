9f4ff70b014ae3295c511700319f91310a08348c
Tea Zlatarevic <tea.zlatarevic@gmail.com>
2021-04-01 15:50:59 +0200

    Solution for the example 3

---
+++ ex03/src/GetRequestErrorHandling.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+
+class GetRequestErrorHandling extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { totalReactPackages: null, errorMessage: null};
+    }
+
+    componentDidMount() {
+        fetch('https://api.npms.io/v2/invalid-url')
+        .then(async response => {
+            const data = await response.json();
+            if(!response.ok) {
+                const error = (data && data.message) || response.statusText;
+                return Promise.reject(error);
+            }
+            this.setState({ totalReactPackages: data.total })
+        })
+        .catch(error => {
+            this.setState({ errorMessage: error.toString() });
+            console.error('There was an error!', error);
+        });
+    }
+
+    render() {
+        const { errorMessage } = this.state;
+        return (
+            <div className="card text-center m-3">
+                <h5 className="card-header">GET Request with Error Handling</h5>
+                <div className="card-body">
+                    Error message: {errorMessage}
+                </div>
+            </div>
+        );
+    }
+
+}
+
+export default GetRequestErrorHandling;
