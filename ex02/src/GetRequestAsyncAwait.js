465c2435f68c927c9687c489da92f2eff587ce5f
Tea Zlatarevic <tea.zlatarevic@gmail.com>
2021-04-01 16:16:30 +0200

    Fixing the example 2

--- ex02/src/GetRequestAsyncAwait.js
+++ ex02/src/GetRequestAsyncAwait.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class GetRequestAsyncAwait extends Component {
+class GetRequestAsyncAwait extends React.Component {
     constructor(props) {
         super(props);
         this.state = { totalReactPackages: null };
